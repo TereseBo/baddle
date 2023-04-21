5a4482cf9ec428168174ae4e2d312e8f6bfdcb00
TereseBo <terese.boderus@gmail.com>
2023-04-21 15:07:26 +0200

    moved warp initialise after states used to set it

--- src/contexts/warpContext.js
+++ src/contexts/warpContext.js
@@ -3,10 +3,9 @@ import { createContext, useState } from "react";
 export const WarpContext=createContext()
 
 export function WarpProvider({children}){
-
-    const [warp, setWarp]=useState([[],[]])
     const [threads, setThreads]=useState(38)
     const [draftheight, setDraftheight]=useState(50)
+    const [warp, setWarp]=useState([new Array(threads/2).fill('',0,threads/2),new Array(threads/2).fill('',0,threads/2)])
 
     return(
         <WarpContext.Provider value={{warp, setWarp,threads, setThreads, draftheight, setDraftheight}}>
