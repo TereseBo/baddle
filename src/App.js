4fa689d2630b546f59b75096036f738232021911
TereseBo <terese.boderus@gmail.com>
2023-04-21 13:49:43 +0200

    Added import of warpcontext and router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 //components
 import Banddraft from './pages/Banddraft';
+import Preferences from './pages/Preferences';
+
+//context
+import { WarpProvider } from './contexts/warpContext';
 
 function App() {
   return (
-    <div className="App">
-<Banddraft/>
-    </div>
+
+    <WarpProvider>
+      <BrowserRouter>
+        <header className="App-header">
+          <Preferences />
+          <Link to="/">Start</Link>
+          <Link to="/draft">Draft</Link>
+        </header>
+        <Routes>
+          <Route path='/draft' element={<Banddraft />} />
+        </Routes>
+      </BrowserRouter>
+    </WarpProvider>
+
   );
 }
 
