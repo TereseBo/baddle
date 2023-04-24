8244a079007b6ee8b6e45090e175bba91a9e9fc7
TereseBo <terese.boderus@gmail.com>
2023-04-24 14:53:20 +0200

    Removal of console.logs

--- src/components/Heddlemove.js
+++ src/components/Heddlemove.js
@@ -1,13 +1,10 @@
 import Keybutton from "./Keybutton";
 
 export default function ({ rowNr }) {
-    console.log(rowNr%2)
-
-
     {
         return(
-        rowNr % 2 == 1 ? (<div><Keybutton text="Upp" /><Keybutton active={true}  text="Ner" /></div>) :
-        (<div><Keybutton active={true} text="Upp" /><Keybutton text="Ner" /></div>)
+        rowNr % 2 == 1 ? (<div><Keybutton text="↑" /><Keybutton active={true}  text="↓" /></div>) :
+        (<div><Keybutton active={true} text="↑"/><Keybutton text="↓" /></div>)
         )
     }
 
