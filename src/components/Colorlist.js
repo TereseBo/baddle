1167f95578208ee880ff7ed79e8a4e5fd6f45965
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-05-02 08:41:28 +0200

    Added box for colordisplay

---
+++ src/components/Colorlist.js
@@ -0,0 +1,11 @@
+import Colorcomp from "./Colorcomp"
+export default function Colorlist({ colors, label, name }) {
+    return (
+        <div>
+            <h3>{label}</h3>
+            {colors.map((color, index) => (
+                <Colorcomp key={index+name} color={color} />
+            ))}
+        </div>
+    )
+}
