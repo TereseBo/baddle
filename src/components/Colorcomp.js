1a9d9aa6bc5cc424fc1bcfaa0e74d07c848cf8e1
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-05-02 08:44:04 +0200

    Added onClick param for colorchange

--- src/components/Colorcomp.js
+++ src/components/Colorcomp.js
@@ -1,9 +1,9 @@
 import Thread from './Thread.js'
 
-export default function Colorcomp({color}){
+export default function Colorcomp({color, onClick}){
     return(
         <div>
-            <Thread color={color}/>
+            <Thread color={color} onClick={onClick}/>
         </div>
     )
 }
--- src/components/Colorlist.js
+++ src/components/Colorlist.js
@@ -1,5 +1,5 @@
 import Colorcomp from "./Colorcomp"
-export default function Colorlist({ colors, label, name }) {
+export default function Colorlist({ colors, label, name, onClick }) {
     return (
         <div>
             <h3>{label}</h3>
