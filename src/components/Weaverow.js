e4c08f4a0031c076da6cef975beb940bebb65347
TereseBo <terese.boderus@gmail.com>
2023-08-30 12:17:31 +0200

    removed unneccessary state

--- src/components/Weaverow.js
+++ src/components/Weaverow.js
@@ -1,5 +1,5 @@
 //dependencies
-import { useContext, useState } from "react";
+import { useContext } from "react";
 //contexts
 import { WarpContext } from '../contexts/warpContext'
 //components
@@ -13,15 +13,21 @@ import useToggle from "../hooks/useToggle";
 export default function Weaverow({ rowNr }) {
     const { warp } = useContext(WarpContext)
     const [holes, toggleHoles] = useToggle(!(rowNr % 2))
-    const [pattern] = useState(holes ? warp[0] : warp[1])
 
     function clickHandler() {
         toggleHoles()
     }
     return (
         <div key={`row-${rowNr}-container`} className={!holes ? 'Weaverow holes' : 'Weaverow slits'}>
-            {pattern.map((cell1, index) =>
-                (<Thread color={cell1} key={`row-${rowNr}thread-${index}`} />))}
+
+            {holes ? (
+                warp[0].map((cell1, index) =>
+                    (<Thread color={cell1} key={`row-${rowNr}thread-${index}`} />))
+            ) : (
+                warp[1].map((cell1, index) =>
+                    (<Thread color={cell1} key={`row-${rowNr}thread-${index}`} />))
+            )
+            }
             <Heddlemove rowNr={rowNr} zRow={holes} clickHandler={clickHandler} />
         </div >
     )
