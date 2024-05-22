65248dc78ce6e63d26212fac6b21f8fa7e5dda40
TereseBo <terese.boderus@gmail.com>
2024-05-22 07:11:35 +0200

    Formatting

--- src/contexts/warpContext.js
+++ src/contexts/warpContext.js
@@ -7,8 +7,12 @@ export const WarpContext = createContext()
 export function WarpProvider({ children }) {
     const [warpthreads, setWarpthreads] = useState(threads)
     const [draftheight, setDraftheight] = useState(draftRows)
-    const [warp, setWarp] = useState([new Array(warpthreads / 2).fill('', 0, warpthreads / 2), new Array(warpthreads / 2).fill('', 0, warpthreads / 2)] )
+    const [warp, setWarp] = useState([new Array(warpthreads / 2).fill('', 0, warpthreads / 2), new Array(warpthreads / 2).fill('', 0, warpthreads / 2)])
     const [weaveArea, setWeaveArea] = useState(() => { return new Array(draftheight).fill('', 0, draftheight) })
+    //TODO: Clenaup
+    //Replace alla instances of warpthreads/2
+    //Handle input of uneven numbers in threads
+
 
     useEffect(() => {
         console.log('I reset weave height')
@@ -16,46 +20,22 @@ export function WarpProvider({ children }) {
 
     }, [draftheight])
     useEffect(() => {
-        //const lengthDiff=warpthreads / 2 < warp[0].length
+     
         console.log('I re ran warp')
-        setWarp(prevWarp=>{
-            const lengthDiff=(warpthreads / 2 )- prevWarp[0].length
+        setWarp(prevWarp => {
+            const lengthDiff = (warpthreads / 2) - prevWarp[0].length
 
             console.log('warpthreads:', warpthreads, 'arr.length', warp[0].length, 'diff:', lengthDiff)
-            let newValue=[]
-            debugger
-            lengthDiff<0? 
-            newValue=[prevWarp[0].toSpliced(warpthreads/2), prevWarp[1].toSpliced(warpthreads/2)]
-            :
-            newValue=[prevWarp[0].concat(new Array(lengthDiff).fill('', 0, lengthDiff)), prevWarp[1].concat(new Array(lengthDiff).fill('', 0, lengthDiff))]
+            let newValue = []
+          
+
+            lengthDiff < 0 ?
+                newValue = [prevWarp[0].toSpliced(warpthreads / 2), prevWarp[1].toSpliced(warpthreads / 2)]
+                :
+                newValue = [prevWarp[0].concat(new Array(lengthDiff).fill('', 0, lengthDiff)), prevWarp[1].concat(new Array(lengthDiff).fill('', 0, lengthDiff))]
             console.log(newValue)
             return newValue
         })
-        
-        //(lengthDiff<0)?setWarp(()=>{return[warp[0].toSpliced(0,lengthDiff), warp[1].toSpliced(0,lengthDiff)] }):setWarp(()=>{return[warp[0], new Array(lengthDiff).fill('',0)] })
-
-/*         let copy = []
-
-        if (warpthreads / 2 < warp[0].length) {
-            copy = warp.map((item) => {
-                return item.toSpliced(warpthreads / 2)
-            })
-        } else if (warpthreads / 2 > warp[0].length) {
-            copy = warp.map((item) => {
-                return item.map((item2, index) => {
-                    while (index < warpthreads / 2) {
-                        item.push('')
-                    }
-                    return item
-                })
-            })
-        }
-        setWarp(copy)
-        console.log(copy)
-        console.log(warpthreads) */
-
-
-
 
     }, [warpthreads])
 
